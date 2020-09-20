a7dba3c014372ef1eca885a7fb098ee1e5c54aa4
Sudarshan Giri <41857483+SudarshanGiri@users.noreply.github.com>
2020-09-20 21:59:24 +0545

    Updates

---
+++ precache-manifest.2ff59c0d577d215e8a0eeac53ff41d3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c136e9f8699149ea413f88e8bd6690c",
+    "url": "/React--The-Burger-Builder/tree/deploy/index.html"
+  },
+  {
+    "revision": "0fbe9521fc638882400a",
+    "url": "/React--The-Burger-Builder/tree/deploy/static/css/main.cfa229da.chunk.css"
+  },
+  {
+    "revision": "a7659ddedf2974f55f44",
+    "url": "/React--The-Burger-Builder/tree/deploy/static/js/2.c22f3a99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React--The-Burger-Builder/tree/deploy/static/js/2.c22f3a99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fbe9521fc638882400a",
+    "url": "/React--The-Burger-Builder/tree/deploy/static/js/main.1c842776.chunk.js"
+  },
+  {
+    "revision": "9ba017b45c898f3aa87c",
+    "url": "/React--The-Burger-Builder/tree/deploy/static/js/runtime-main.868c4816.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/React--The-Burger-Builder/tree/deploy/static/media/burger-logo.b8503d26.png"
+  }
+]);
